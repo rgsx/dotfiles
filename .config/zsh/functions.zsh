daeb5c3afd3b7861771f979821ae1f621b8c949e
J. Roberto González <67397118+rgsx@users.noreply.github.com>
2026-07-20 15:42:33 -0600

    update + eza

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -18,8 +18,8 @@ mkdir-move() {
 
 ## Tree view for current folder with all info
 list() {
-   eza --oneline --long --tree --classify=always --color=auto --icons=always --hyperlink --list-dirs --level=1 --sort=type --classify=always --group-directories-first --git --no-permissions --no-filesize --no-user --no-time . $1
-}
+  eza -1lTA --level=1 --classify=always --color=auto --icons=always --hyperlink=auto --sort=type --git --no-permissions --no-user --no-time --group-directories-first . $1
+  }
 
 ## Safe delete with trash
 delete() {
@@ -107,7 +107,6 @@ convertToMp4() {
 
 ## Homebrew full maintance function
 brewup() {
-  export PATH="/opt/homebrew/bin:$PATH"
   # Colours
   local red="\033[0;31m"
   local green="\033[0;32m"
